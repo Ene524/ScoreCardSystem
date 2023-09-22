c210b04bc12b688219c3bbf87f273faefc8cab96
Enes Karakuş <eneskarakus44@outlook.com>
2023-09-22 14:48:38 +0300

    Update

--- app/Http/Controllers/Employee/Api/DashboardController.php
+++ app/Http/Controllers/Employee/Api/DashboardController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Employee\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Permit;
-use App\Models\Workday;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
+
+    public function login(Request $request){
+
+    }
     public function getWorkdays(Request $request)
     {
         return auth('employee')->user()->id;
@@ -26,17 +26,19 @@ class DashboardController extends Controller
 //        );
     }
 
-    public function getPermits(Request $request)
-    {
-        return response()->json(
-            [
-                'permits' => Permit::with([
-                    'employee'
-                ])->whereBetween('start_date', [$request->start_date, $request->end_date])
-                    ->where('employee_id', auth()->user()->id)
-                    ->where('status', '1')
-                    ->get()
-            ]
-        );
-    }
+//    public function getPermits(Request $request)
+//    {
+//        return response()->json(
+//            [
+//                'permits' => Permit::with([
+//                    'employee'
+//                ])->whereBetween('start_date', [$request->start_date, $request->end_date])
+//                    ->where('employee_id', auth()->user()->id)
+//                    ->where('status', '1')
+//                    ->get()
+//            ]
+//        );
+//    }
+
+
 }
