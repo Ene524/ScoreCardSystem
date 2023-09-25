d9b27d593d73b1f521206c64243e8d1ea142993b
Enes Karakuş <mail.eneskarakus@gmail.com>
2023-09-25 23:22:18 +0300

    update
    Signed-off-by: Enes Karakuş <mail.eneskarakus@gmail.com>

--- app/Http/Requests/User/EmployeeRequest.php
+++ app/Http/Requests/User/EmployeeRequest.php
@@ -23,11 +23,31 @@ class EmployeeRequest extends FormRequest
     {
         return [
             'full_name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
+            'email' => 'required|email|unique:employees,email',
             'department_id' => 'required|integer',
             'position_id' => 'required|integer',
             'salary_id' => 'required|integer',
             'employment_date' => 'required|date',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'full_name.required' => 'Ad Soyad alanı zorunludur.',
+            'full_name.string' => 'Ad Soyad alanı metin tipinde olmalıdır.',
+            'full_name.max' => 'Ad Soyad alanı en fazla 255 karakter olmalıdır.',
+            'email.required' => 'E-posta alanı zorunludur.',
+            'email.email' => 'E-posta alanı e-posta formatında olmalıdır.',
+            'email.unique' => 'E-posta alanı daha önce kayıt edilmiş.',
+            'department_id.required' => 'Departman alanı zorunludur.',
+            'department_id.integer' => 'Departman alanı sayı tipinde olmalıdır.',
+            'position_id.required' => 'Pozisyon alanı zorunludur.',
+            'position_id.integer' => 'Pozisyon alanı sayı tipinde olmalıdır.',
+            'salary_id.required' => 'Maaş alanı zorunludur.',
+            'salary_id.integer' => 'Maaş alanı sayı tipinde olmalıdır.',
+            'employment_date.required' => 'İşe Giriş Tarihi alanı zorunludur.',
+            'employment_date.date' => 'İşe Giriş Tarihi alanı tarih tipinde olmalıdır.',
         ];
     }
 }
