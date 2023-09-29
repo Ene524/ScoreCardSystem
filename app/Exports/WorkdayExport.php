69ee082e3f51b115e6be23472bf6b7694e3ca8ee
Enes Karakuş <mail.eneskarakus@gmail.com>
2023-09-29 23:05:11 +0300

    Update

--- app/Exports/WorkdayExport.php
+++ app/Exports/WorkdayExport.php
@@ -35,7 +35,8 @@ class WorkdayExport implements FromCollection, WithHeadings
             ')
             ->join('employees', 'employees.id', '=', 'workdays.employee_id')
             ->join('workday_types', 'workday_types.id', '=', 'workdays.workday_type_id')
-//            ->whereBetween('workdays.start_date', [request()->start_date, request()->end_date])
+            //            ->whereBetween('workdays.start_date', [request()->start_date, request()->end_date])
+            //Duruma göre bu alan da kontrol edilecek
             ->get();
     }
 }
