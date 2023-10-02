8bc9b64e15b32487f8baba47cb9ac9aa55cd0e86
Enes Karakuş <eneskarakus44@outlook.com>
2023-10-02 12:40:57 +0300

    Update

--- resources/views/user/modules/workday/report/components/script.blade.php
+++ resources/views/user/modules/workday/report/components/script.blade.php
@@ -1,4 +1,6 @@
 <script src="{{ asset('assets/bower_components/select2/dist/js/select2.min.js') }}"></script>
+<script src="https://cdn.sheetjs.com/xlsx-0.19.3/package/dist/xlsx.full.min.js"></script>
+
 
 <script>
     $(document).ready(function () {
@@ -26,8 +28,8 @@
             var year = today.getFullYear();
             var month = (today.getMonth() + 1).toString().padStart(2, "0");
             var day = today.getDate().toString().padStart(2, "0");
-            var startDate = year + "-" + month + "-" + day +" "+ "00:00"
-            var endDate = year + "-" + month + "-" + day +" "+ "23:59"
+            var startDate = year + "-" + month + "-" + day + " " + "00:00"
+            var endDate = year + "-" + month + "-" + day + " " + "23:59"
 
             $("#start_date").val(startDate);
             $("#end_date").val(endDate);
@@ -35,7 +37,6 @@
         });
 
         $("#selectThisWeek").click(function () {
-
             var today = new Date();
             var firstDayOfWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay());
             var lastDayOfWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + (6 - today.getDay()));
@@ -43,19 +44,59 @@
             var year = firstDayOfWeek.getFullYear();
             var month = (firstDayOfWeek.getMonth() + 1).toString().padStart(2, "0");
             var day = firstDayOfWeek.getDate().toString().padStart(2, "0");
-            var startOfWeek = year + "-" + month + "-" + day +" "+ "00:00"
+            var startOfWeek = year + "-" + month + "-" + day + " " + "00:00"
 
             year = lastDayOfWeek.getFullYear();
             month = (lastDayOfWeek.getMonth() + 1).toString().padStart(2, "0");
             day = lastDayOfWeek.getDate().toString().padStart(2, "0");
-            var endOfWeek = year + "-" + month + "-" + day +" "+ "23:59";
+            var endOfWeek = year + "-" + month + "-" + day + " " + "23:59";
 
             $("#start_date").val(startOfWeek);
             $("#end_date").val(endOfWeek);
             $("#quickSelectModal").modal("toggle");
         });
 
+        $("#selectThisMonth").click(function () {
+            var today = new Date();
+            var firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+            var lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+            var year = firstDayOfMonth.getFullYear();
+            var month = (firstDayOfMonth.getMonth() + 1).toString().padStart(2, "0");
+            var day = firstDayOfMonth.getDate().toString().padStart(2, "0");
+            var startOfMonth = year + "-" + month + "-" + day + " " + "00:00"
+
+            year = lastDayOfMonth.getFullYear();
+            month = (lastDayOfMonth.getMonth() + 1).toString().padStart(2, "0");
+            day = lastDayOfMonth.getDate().toString().padStart(2, "0");
+            var endOfMonth = year + "-" + month + "-" + day + " " + "23:59";
+
+            $("#start_date").val(startOfMonth);
+            $("#end_date").val(endOfMonth);
+            $("#quickSelectModal").modal("toggle");
+        });
+
+        $("#selectLastMonth").click(function () {
+            var today = new Date();
+            var firstDayOfMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+            var lastDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+
+            var year = firstDayOfMonth.getFullYear();
+            var month = (firstDayOfMonth.getMonth() + 1).toString().padStart(2, "0");
+            var day = firstDayOfMonth.getDate().toString().padStart(2, "0");
+            var startOfMonth = year + "-" + month + "-" + day + " " + "00:00"
+
+            year = lastDayOfMonth.getFullYear();
+            month = (lastDayOfMonth.getMonth() + 1).toString().padStart(2, "0");
+            day = lastDayOfMonth.getDate().toString().padStart(2, "0");
+            var endOfMonth = year + "-" + month + "-" + day + " " + "23:59";
+
+            $("#start_date").val(startOfMonth);
+            $("#end_date").val(endOfMonth);
+            $("#quickSelectModal").modal("toggle");
+        });
     })
+
 
 
 
@@ -94,7 +135,24 @@
     }
 
     function calcSalary(totalHours, salary) {
-        var perHourAmount = salary / 30 / 9;
+        var perHourAmount = salary / 26 / 9;
         return totalHours * perHourAmount;
     };
+</script>
+
+<script>
+    const exportButton = $('#downloadExcel');
+    const table = document.getElementById('TotalWorkHours');
+
+    exportButton.click(function () {
+      if (table.rows.length> 1) {
+        const ws = XLSX.utils.table_to_sheet(table);
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'SheetJS');
+        XLSX.writeFile(wb, 'TotalWorkHours.xlsx');
+      }
+      else{
+        alert("Lütfen önce filtreleme yapınız.");
+      }
+    });
 </script>
