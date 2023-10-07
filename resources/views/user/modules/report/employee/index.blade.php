aab9d8835bd7778771a89c400a8177ddd4ac0672
Enes Karakuş <eneskarakus44@outlook.com>
2023-10-07 17:10:21 +0300

    Update
    Signed-off-by: Enes Karakuş <eneskarakus44@outlook.com>

--- resources/views/user/modules/report/employee/index.blade.php
+++ resources/views/user/modules/report/employee/index.blade.php
@@ -6,29 +6,28 @@
         <div class="col-md-12">
             <div class="box box-primary">
                 <div class="box-header">
-                    <h3 class="box-title">Çalışma Raporu</h3>
+                    <h3 class="box-title">Personel Raporu</h3>
                 </div>
                 <div class="box-body">
-                    <div class="alert alert-info alert-dismissible">
-                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                        <h4><i class="icon fa fa-info"></i> Alert!</h4>
-                        Info alert preview. This alert is dismissable.
-                        </div>
-
-
+                    <div class="col-md-6">
+                        Personel Raporunu excel olarak indirebilirsiniz
+                    </div>
+                    <div class="col-md-6">
+                        <a href="{{ route('user.repo') }}" class="btn btn-success pull-right">Excel
+                            İndir</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    @include('user.modules.report.totalHour.modals.modal_workday_report_quick_select')
+        @include('user.modules.report.totalHour.modals.modal_workday_report_quick_select')
 
-@endsection
+    @endsection
 
-@section('customStyle')
-    @include('user.modules.report.totalHour.components.style')
-@endsection
+    @section('customStyle')
+        @include('user.modules.report.totalHour.components.style')
+    @endsection
 
-@section('customScript')
-@include('user.modules.report.totalHour.components.script')
-@endsection
+    @section('customScript')
+        @include('user.modules.report.totalHour.components.script')
+    @endsection
